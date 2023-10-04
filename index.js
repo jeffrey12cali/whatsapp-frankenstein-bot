b7a27d3aaa983c938684e55031a1b9fd99cda8a4
Jeffrey García <jeffrey12cali@javerianacali.edu.co>
2023-10-04 19:06:08 +0200

    ytaud with url and callate

--- index.js
+++ index.js
@@ -36,6 +36,27 @@ const isTime1LongerThanTime2 = (time1, time2) => {
     }
 }
 
+const sendYtAudio = (url, id, msg) => {
+    subProcess.execSync(`yt-dlp --extract-audio --audio-format mp3 -o "./audio-output/%(id)s.mp3" ${url}`, (err, stdout, stderr) => {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+    });
+    try {
+        (async () => {
+            const media = MessageMedia.fromFilePath(`./audio-output/${id}.mp3`);
+            await msg.reply(media);
+            setTimeout(() => {
+                subProcess.execSync(`rm audio-output/${id}.mp3`);
+            }, 100);
+        })();
+    }
+    catch(err) {
+        msg.reply(err);
+    }
+}
+
 client.on('qr', qr => {
     qrcode.generate(qr, {small: true});
 });
@@ -50,34 +71,40 @@ client.on('message', async msg => {
     }
     else if (msg.body.startsWith('!ytaud ')) {
         let query = msg.body.substring(7);
-        youtubesearchapi
-            .GetListByKeyword(query, false, 5, [{type: "video"}])
-            .then( res => {
-                if (!isTime1LongerThanTime2(res.items[0].length.simpleText, "30:00")) {
-                    let id = res.items[0].id;
-                    subProcess.execSync(`yt-dlp --extract-audio --audio-format mp3 -o "./audio-output/%(id)s.mp3" https://www.youtube.com/watch?v=${id}`, (err, stdout, stderr) => {
-                        if (err) {
-                            console.error(err);
-                            process.exit(1);
-                        }
-                    });
-                    try {
-                        (async () => {
-                            const media = MessageMedia.fromFilePath(`./audio-output/${id}.mp3`);
-                            await msg.reply(media);
-                            setTimeout(() => {
-                                subProcess.execSync(`rm audio-output/${id}.mp3`);
-                            }, 100);
-                        })();
+        let isYoutubeUrlTrad = query.includes('youtube.com/watch?v=');
+        let isYoutubeUrlShort = query.includes('youtu.be/');
+        if (isYoutubeUrlTrad || isYoutubeUrlShort) {
+            let id = '';
+            if (isYoutubeUrlTrad) {
+                id = query.match(/=(.)*/gm).toString().substring(1);
+            }
+            else if (isYoutubeUrlShort) {
+                id = query.match(/.be\/(.)*\?/gm).toString().substring(4).slice(0, -1);
+            }
+            youtubesearchapi
+                .GetVideoDetails(id)
+                .then( res => {
+                    if (!res.isLive) {
+                        sendYtAudio(query, id, msg);
                     }
-                    catch(err) {
-                        msg.reply(err);
+                    else {
+                        msg.reply("Video is live.");
                     }
-                }
-                else {
-                    msg.reply("Video too long. Try with another search query.");
-                }
-            });
+                });
+        }
+        else {
+            youtubesearchapi
+                .GetListByKeyword(query, false, 5, [{type: "video"}])
+                .then( res => {
+                    if (!res.items[0].isLive && !isTime1LongerThanTime2(res.items[0].length.simpleText, "30:00")) {
+                        let id = res.items[0].id;
+                        sendYtAudio(`https://www.youtube.com/watch?v=${id}`, id, msg);
+                    }
+                    else {
+                        msg.reply("Video too long. Try with another search query.");
+                    }
+                });
+        }
     }
     else if (msg.body.startsWith('!ytvid ')) {
         msg.reply("Monday left me broken");
@@ -117,6 +144,16 @@ client.on('message', async msg => {
                     msg.reply("Video too long. Try with another search query.");
                 }
             });*/
+    }
+    else if (msg.body === '!callate') {
+        const media = MessageMedia.fromFilePath(`./audio/callate.ogg`);
+        if (msg.hasQuotedMsg) {
+            let source_msg = await msg.getQuotedMessage();
+            await source_msg.reply(media);
+        }
+        else {
+            await msg.reply(media);
+        }
     }
 });
 
